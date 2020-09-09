6b16cd5e087333a0357d195f67d102d447639a41
Wajid Ali <69789862+Wajid-Ali12@users.noreply.github.com>
2020-09-09 17:15:19 +0500

    Test

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './components/Navbar'; 
+import landing from './components/Landing'; 
+import Login from './components/Login'; 
+import Register from './components/Register'; 
+import Profile from './components/Profile'; 
+function App() {
+  return (
+   
+      <Router>
+         <div className="App">
+           <Navbar/>
+           <Route exact path="/" component={landing}/>
+         <div className="container">
+           <Route exact path="/register" component={Register}/>
+           <Route exact path="/login" component={Login}/>
+           <Route exact path="/profile" component={Profile}/>
+ 
+         </div>
+         </div>
+
+
+      </Router>
+    
+  );
+}
+
+export default App;
